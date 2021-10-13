c08d04c9fb5e38405e248a9ecbf4e8254ba26c52
radinayoana8 <>
2021-10-13 14:39:58 +0000

    i

---
+++ src/exercise1/Baum.java
@@ -0,0 +1,14 @@
+package exercise1;
+
+public class Baum {
+
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("***");
+        System.out.println(" *****");
+        System.out.println("*******");
+        System.out.println("*********");
+        System.out.println("***");
+    }
+
+}
---
+++ src/exercise1/HalloFDIBA.java
@@ -0,0 +1,9 @@
+package exercise1;
+
+public class HalloFDIBA {
+
+    public static void main(String[] args) {
+        System.out.println("Hallo FDIBA!");
+    }
+
+}
